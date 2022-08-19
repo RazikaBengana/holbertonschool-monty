29d57152a41442a9ca69b6e0a687b579883b3f68
noemiepham <huynhtieuquy@gmail.com>
2022-08-19 13:03:50 +0200

    _pint optioncode

---
+++ _pint.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+ * _pint - prints the value at the top of the stack
+ * @head_stack: pointer to node
+ * @line_number:number of line option code
+ */
+void _pint(stack_t **head_stack, unsigned int line_number)
+{
+	int index = 0;
+	stack_t *cursor;
+
+	cursor = *head_stack;
+
+	UNUSED(line_number);
+	while (cursor)
+	{
+		if (cursor != NULL)
+		{
+			printf("%d\n", cursor->n->next);
+		}
+		else
+		{
+			printf("L%d: can't pint, stack empty\n", cursor->n->next);
+			exit(EXIT_FAILURE);
+		}
+		index++;
+		cursor = cursor->next;
+	}
+}
