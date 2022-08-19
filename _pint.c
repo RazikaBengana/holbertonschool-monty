65cb90b232eb8c33d3fde5bce3b1068afb19783d
noemiepham <huynhtieuquy@gmail.com>
2022-08-19 13:16:43 +0200

    corrige error invalid argument

--- _pint.c
+++ _pint.c
@@ -12,16 +12,16 @@ void _pint(stack_t **head_stack, unsigned int line_number)
 	cursor = *head_stack;
 
 	UNUSED(line_number);
+	if (cursor == NULL)
+		{
+			printf("L%d: can't pint, stack empty\n", cursor->n);
+			exit(EXIT_FAILURE);
+		}
 	while (cursor)
 	{
 		if (cursor != NULL)
 		{
 			printf("%d\n", cursor->n);
-		}
-		else
-		{
-			printf("L%d: can't pint, stack empty\n", cursor->n);
-			exit(EXIT_FAILURE);
 		}
 		index++;
 		cursor = cursor->next;
