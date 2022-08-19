f4ba28f1cbf46e364779605ccb57b7692bbcbdf5
noemiepham <huynhtieuquy@gmail.com>
2022-08-19 10:55:04 +0200

    betty style

--- free_memory.c
+++ free_memory.c
@@ -1,1 +1,16 @@
+#include "monty.h"
+/**
+ * free_memory - free memory
+ * @head: top stack
+ */
+void free_memory(stack_t *head)
+{
+	stack_t *tmp;
 
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
